a4744b61bc81673c576487e913f60b697442f260
maximalfactor <nabbvik.max@gmail.com>
2024-05-01 13:39:00 +0300

    remove fetching from app.js

--- App.js
+++ App.js
@@ -12,34 +12,8 @@ import { useState } from "react"
 import { Text } from "react-native"
 
 
-async function fetchItAll(fetchLimit) {
-  let news = []
-  news.push(await HsFetcher(fetchLimit))
-  news.push(await NYTFetcher(fetchLimit))
-  news.push(await BBCFetcher(fetchLimit))
-  news.push(await MtvProto(fetchLimit))
-  return news
-}
 
 export default function App() {
-  const [allNews, setAllNews] = useState([])
-  if(allNews.length == 0) {
-    fetchItAll(20).then(news => {
-      var temp = []
-      for(let source of news) {
-        temp = [...temp, ...source]
-      }
-      setAllNews(temp)
-      })
-  }
-  /*
-  NOTE: allNews will be [] on the first time app is loaded. When all the fetches arrive the app will re-render and allNews will have the data. Removing the above if statement
-  would cause the program to infinitely fetch all the feeds causing us to get ip-banned.
-  */
-  
-
-
-  return allNews.length != 0 ? (
     <ThemeProvider>
         <PaperProvider>
           <FontSizeProvider>
@@ -50,6 +24,4 @@ export default function App() {
           </FontSizeProvider>
         </PaperProvider>
     </ThemeProvider>
-  ):
-  <Text>Loading...</Text>
 }
