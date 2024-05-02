b40788d0a32007543e9d486644699265036eb342
maximalfactor <nabbvik.max@gmail.com>
2024-05-02 21:34:47 +0300

    shuffle news

--- components/filterNews.js
+++ components/filterNews.js
@@ -5,6 +5,13 @@ import BBCFetcher from "./bbcProto"
 import NYTFetcher from "./nytProto"
 import MtvProto from "./mtvproto"
 
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
 export async function filterNews(fetchLimit) {
     try {
         const hs = await HsFetcher(fetchLimit)
@@ -13,7 +20,7 @@ export async function filterNews(fetchLimit) {
         const mtv = await MtvProto(fetchLimit)
 
         const news = [...hs, /*...bbc,*/ ...nyt, ...mtv]
-
+        shuffle(news)
         const filteredNews = {
             "Talous": news.filter(newsItem => newsItem["categories:"] === "Talous"),
             "Urheilu": news.filter(newsItem => newsItem["categories:"] === "Urheilu"),
